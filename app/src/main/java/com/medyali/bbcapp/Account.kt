2078a35c56fac5a97fccddaea25dfbdb0b693996
Muhammad Ali <72379752+muhammadAli-create15@users.noreply.github.com>
2022-07-21 11:13:47 +0300

    My BBC app

--- app/src/main/java/com/medyali/bbcapp/Account.kt
+++ app/src/main/java/com/medyali/bbcapp/Account.kt
@@ -3,15 +3,40 @@ package com.medyali.bbcapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class Account : AppCompatActivity() {
+    // private EditText username,email,password,password1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
-        val createAccount=findViewById<Button>(R.id.button1)
+        //  checkCredentials();
+
+
+        var username = findViewById<EditText>(R.id.edtUsername)
+        var email = findViewById<EditText>(R.id.edtEmail)
+        var password = findViewById<EditText>(R.id.editPassword)
+        val confirmPassword = findViewById<EditText>(R.id.editPassword1)
+
+        checkCredentials();
+        val createAccount = findViewById<Button>(R.id.button1)
         createAccount.setOnClickListener {
-       Toast.makeText(this,"Account has been created successfully",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Account has been created successfully", Toast.LENGTH_LONG).show()
         }
     }
+
+    private fun checkCredentials() {
+
+
+    }
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/medyali/bbcapp/MainActivity.kt
+++ app/src/main/java/com/medyali/bbcapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.medyali.bbcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+/*import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.medyali.bbcapp.Fragments.*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+     //Declare the instances of each fragment
+       /* val homeFragment=HomeFragment()
+        val newsFragment=NewsFragment()
+        val sportsFragment=SportsFragment()
+        val worklifeFragment=WorkLifeFragment()
+        val weatherFragment=WeatherFragment()
+        val aboutFragment=AboutFragment()
+        //Set the initial fragment that shows when we create the app
+
+       setCurrentFragment(homeFragment)*/
+        //Respond to the clicks on our different menu items and switch to the corresponding fragments
+      // var bottomNavigationView=findViewById<ConstraintLayout>(R.id.bottomNav)
+       // bottomNavigationView.
+    //}
+ /*   private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container,fragment)
+            commit()*/
+        }
 }
+
+
+
+
